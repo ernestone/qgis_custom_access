a9666617e1da21d58b4e5ec5bda3b45950a54836
Ernesto Arredondo <ARREDOER@port.apb.es>
2017-05-30 10:00:26 +0200

    - Corregido error por el que al lanzar QGIS leía los parámetros del BAT propio de la APB

--- config/qgis/qgis_apb.bat
+++ config/qgis/qgis_apb.bat
@@ -4,4 +4,4 @@ call "%~dp0\..\env\apb_qgis_env.bat" "%~1" "%~2" %~3
 
 pushd "%OSGEO4W_ROOT%"\bin
 
-start "%QGISNAME%" /B "%OSGEO4W_ROOT%"\bin\%QGISNAME%-bin.exe %*
+start "%QGISNAME%" /B "%OSGEO4W_ROOT%"\bin\%QGISNAME%-bin.exe
