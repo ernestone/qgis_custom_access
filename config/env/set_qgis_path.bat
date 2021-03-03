673c2106a8738808adaa6b4b90049177c6af4394
Ernesto Arredondo Martinez <ernestone@gmail.com>
2021-03-03 10:18:07 +0100

    chgd logic to set qgis_path

--- config/env/set_qgis_path.bat
+++ config/env/set_qgis_path.bat
@@ -1,30 +1,31 @@
 rem =======================================================================
 rem Set the where qgis is installed (OSGEO4W_ROOT)
+rem  - First check if %~1 argument exists
+rem  - Second see if exists relative directory ..\..\..\source_qgis
+rem  - Three see if exists envvar OSGEO4W_ROOT_ALT
+rem  - Last use OSGEO4W_ROOT
 rem =======================================================================
 
-
-if ["%~1"]==[""] (goto :set_osgeo_alt)
-set "OSGEO4W_ROOT=%~1"
-
-:chk_exist
-if DEFINED OSGEO4W_ROOT (
-    if exist "%OSGEO4W_ROOT%" (goto:eof)
+if ["%~1"] NEQ [""] (
+    goto :set_argument
 )
-
-:setDefPth
-rem EAM - Sacado el pushd fuera del condicional ya que %CD% devuelve el valor al parsearse
-pushd %~dp0..\..\..
-set "OSGEO4W_ROOT=%CD%\source_qgis"
-popd
-goto :eof
 
 :set_osgeo_alt
 if defined OSGEO4W_ROOT_ALT (
-    if EXIST "%OSGEO4W_ROOT_ALT%" (
-        goto :set_osgeo_root
-    )
+    set "OSGEO4W_ROOT=%OSGEO4W_ROOT_ALT%"
 )
-goto :chk_exist
 
-:set_osgeo_root
-set "OSGEO4W_ROOT=%OSGEO4W_ROOT_ALT%"
+:set_argument
+if exist "%~1" (
+    set "OSGEO4W_ROOT=%~1"
+)
+
+if DEFINED OSGEO4W_ROOT (
+    if EXIST "%OSGEO4W_ROOT%" (goto :eof)
+)
+
+:set_default
+pushd %~dp0..\..\..
+set "REL_OSGEO4W_PATH=%CD%\source_qgis"
+popd
+set "OSGEO4W_ROOT=%REL_OSGEO4W_PATH%"
